cd172b527150604001714593c42db791bf0b9b0d
duy mõm <103164817+VeolaGoldsonzbc19@users.noreply.github.com>
2022-04-07 11:21:37 +0700

    Create  gatsby-node.js

---
+++ gatsby-node.js/gatsby-node.js
@@ -0,0 +1,37 @@
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions;
+  const typeDefs = `
+    type NewsItem implements Node {
+      id: String!
+      date: Date!
+      locale: String!
+      author: String
+      source: String
+      link: String!
+      title: String!
+      newsType: String!
+    }
+
+    type ServicesAppsCollection implements Node {
+      title: String!
+      links: [AppLinks]
+      author: String
+      authorLink: String
+      description: String!
+      type: String!
+      date: Date!
+      verifiedContract: String
+      openSource: String
+      audit: String
+      testSuite: String
+      ipfsFrontend: String
+    }
+    
+    type AppLinks {
+      name: String
+      link: String
+      icon: String
+    }
+  `;
+  createTypes(typeDefs);
+};
